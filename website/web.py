335ee5d0423bd1771bd33c7827c5d03abfc70519
zhanghedong <zhanghedong@gmail.com>
2015-04-25 14:36:18 +0800

    修改样式

--- website/web.py
+++ website/web.py
@@ -1,12 +1,53 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template, send_from_directory, redirect, url_for,g
+from flask import Blueprint, render_template, send_from_directory, redirect, url_for, g
 import os
 from utils.func import init_logger
 
 LOGGER = init_logger(__name__)
 
 web = Blueprint('web', __name__, template_folder='templates', static_folder='static')
+
+
+def get_ticket():
+    path = './ticket.json'
+    if os.path.isfile(path):
+        fh = open(path)
+        l = fh.read()
+        if l:
+            arr = l.split('|')
+            s = arr[0]
+            t = arr[1]
+            if int(t) + 7000 > int(time.time()):
+                fh.close()
+                return s
+    # access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxc891ebaf608261cb&secret=149f6e83ef1ea173b755ce87c5fb482b'
+    access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + APP_ID + '&secret=' + APP_SECRET
+    ret = requests.get(access_token_url)
+    rs = ret.json()
+    gicket_url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + rs['access_token'] + '&type=jsapi'
+    ret = requests.get(gicket_url)
+    rs = ret.json()
+    m_file = open(path, 'w')
+    m_file.write(rs['ticket'] + '|' + str(int(time.time())))
+    m_file.close()
+    return rs['ticket']
+
+
+def get_weixin_sign():
+    if APP_MODE != 'Local':
+        ticket = get_ticket()
+        sign = Sign(ticket, request.url)
+        sign = sign.sign()
+        sign['appid'] = APP_ID
+    else:
+        sign = {
+            'timestamp': '',
+            'nonceStr': '',
+            'signature': '',
+            'appid': ''
+        }
+    return sign
 
 
 @web.errorhandler
@@ -26,18 +67,21 @@ import website.views
 
 @web.route('/')
 def index():
+    sign = get_weixin_sign()
     g.uri_path = '/'
-    return render_template('index/index.html')
+    return render_template('index/index.html', sign=sign)
 
 
 @web.route('/contact')
 def contact():
-    return render_template('index/contact.html')
+    sign = get_weixin_sign()
+    return render_template('index/contact.html', sign=sign)
 
 
 @web.route('/price')
 def price():
-    return render_template('index/price.html')
+    sign = get_weixin_sign()
+    return render_template('index/price.html', sign=sign)
 
 
 @web.route('/docs')
